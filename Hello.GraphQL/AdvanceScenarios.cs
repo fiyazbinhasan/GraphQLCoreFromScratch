823c6a5d226ab76e8fc59a0b4179192e20b1e9ce
fiyazbinhasan <fiyazhasan@live.com>
2020-03-28 22:44:01 +0600

    3-28-2020 GraphQL Hello World Console

---
+++ Hello.GraphQL/AdvanceScenarios.cs
@@ -0,0 +1,54 @@
+﻿#region More advanced schema and queries
+//  var schema = Schema.For(@"
+//        type Query {
+//            game: Playable
+//        }
+
+//        type Playable {
+//            title: String
+//            price: Decimal
+//            status: String
+//        }",
+//      _ =>
+//      {
+//          _.Types.Include<GameType>();
+//          _.Types.Include<Query>();
+//      }
+//  );
+
+//  var json = await schema.ExecuteAsync(_ =>
+//  {
+//      _.Query = "{ game { title price status } }";
+//  });
+
+//  Console.WriteLine(json);
+
+//  class Query
+//  {
+//      [GraphQLMetadata("game")]
+//      public Playable GetPlayable()
+//      {
+//          return new Playable
+//          {
+//              Title = "Grand Theft Auto V",
+//              Price = 9.99M,
+//              IsOnSale = false
+//          };
+//      }
+//  }
+
+//  class Playable
+//  {
+//      public string Title { get; set; }
+//      public decimal Price { get; set; }
+//      public bool IsOnSale { get; set; }
+//  }
+
+//  [GraphQLMetadata("Playable", IsTypeOf = typeof(Playable))]
+//  class GameType
+//  {
+//      public string Title(Playable playable) => playable.Title;
+//      public decimal Price(Playable playable) => playable.Price;
+//      public string Status(Playable playable) => playable.IsOnSale ? "On Sale" : "Regular Price";
+//  }
+#endregion
---
+++ Hello.GraphQL/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Types;
+using GraphQL.NewtonsoftJson;
+
+namespace Hello.GraphQL
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            /* Schema first approach */
+            var schemaFirst = Schema.For(@"
+                type Query {
+                    hello: String
+                }
+            ");
+
+            /* Code first approach */
+            var codeFirst = new Schema { Query = new HelloWorldQuery() };
+
+            var schemaFirstJson = await schemaFirst.ExecuteAsync(_ =>
+            {
+                _.Query = "{ hello }";
+                _.Root = new { Hello = "world" };
+            }); 
+            
+            var codeFirstJson = await codeFirst.ExecuteAsync(_ =>
+            {
+                _.Query = "{ hello }";
+            });
+
+            Console.WriteLine("\nSchema First Approach\n");
+            Console.WriteLine(schemaFirstJson);
+            Console.WriteLine("\nCode First Approach\n");
+            Console.WriteLine(codeFirstJson);
+        }
+    }
+
+    class HelloWorldQuery : ObjectGraphType
+    {
+        public HelloWorldQuery()
+        {
+            Field<StringGraphType>(
+                name: "hello",
+                resolve: context => "world"
+            );
+        }
+    }
+}
