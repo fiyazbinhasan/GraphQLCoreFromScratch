c027a69d34c119c62d11ddf6518240a20250fcab
fiyazbinhasan <fiyazhasan@live.com>
2020-09-14 23:14:51 +0600

    14-9-2020 Seperate class for GraphQLMiddleware Extensions

---
+++ Web.GraphQL/GraphQLMiddlewareExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Web.GraphQL
+{
+    public static class GraphQLMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseGraphQL(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<GraphQLMiddleware>();
+        }
+
+        public static IServiceCollection AddGraphQL(this IServiceCollection services, Action<GraphQLOptions> action)
+        {
+            return services.Configure(action);
+        }
+    }
+}
